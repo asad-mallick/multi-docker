0c936c0a8125c989abb2ce139de5d7dd70f1a790
asad <asad_mallik@yahoo.co.in>
2022-05-19 13:34:45 +0530

    some changes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,43 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
+
+
+//function App() {
+ // return (
+   // <Router>
+     // <div className="App">
+       // <div>
+         // <Route exact path="/" component={Fib} />
+          //<Route path="/otherpage" component={OtherPage} />
+        //</div>
+     // </div>
+   // </Router>
+ // );
+//}
+
+//export default App;
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
         <div>
           <Route exact path="/" component={Fib} />
           <Route path="/otherpage" component={OtherPage} />
@@ -18,3 +48,4 @@ function App() {
 }
 
 export default App;
+
