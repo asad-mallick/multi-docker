e752363b7a0ef8743539b5aa0aaa9a72b31cb573
asad <asad_mallik@yahoo.co.in>
2022-05-23 17:55:12 +0530

    slight change in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -33,7 +33,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn Docker
+            Learn 
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
