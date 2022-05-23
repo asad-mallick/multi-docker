24bf16433bd9a0631965da9876b180e47bd846cc
asad <asad_mallik@yahoo.co.in>
2022-05-23 15:04:36 +0530

    EBS on multicontainer docker

--- client/src/App.js
+++ client/src/App.js
@@ -33,7 +33,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn DevOps
+            Learn Docker
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
