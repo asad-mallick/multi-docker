067011aa8edc84afc6ae6b765b5dc31894bc14d1
asad <asad_mallik@yahoo.co.in>
2022-05-23 12:04:08 +0530

    minor changes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -33,7 +33,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn DevOps
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
